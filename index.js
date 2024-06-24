dc6ff68a7a87ebdf3175462fb61db8fbdeb96186
mikeyis <44427223+mikeyis@users.noreply.github.com>
2024-06-24 13:58:36 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+const axios = require('axios');
+const { Accessory, Service, Characteristic, uuid } = require('hap-nodejs');
+
+const pluginName = 'homebridge-electricity-spike';
+const platformName = 'ElectricitySpikePlatform';
+
+module.exports = (homebridge) => {
+  homebridge.registerAccessory(pluginName, platformName, ElectricitySpikeAccessory);
+};
+
+class ElectricitySpikeAccessory {
+  constructor(log, config) {
+    this.log = log;
+    this.name = config.name;
+    this.apiUrl = config.apiUrl;
+    this.apiToken = config.apiToken;
+    this.pollingInterval = config.pollingInterval || 60000; // default to 1 minute
+
+    this.spikeDetected = false;
+
+    this.informationService = new Service.AccessoryInformation()
+      .setCharacteristic(Characteristic.Manufacturer, 'Your Company')
+      .setCharacteristic(Characteristic.Model, 'Electricity Spike Detector')
+      .setCharacteristic(Characteristic.SerialNumber, '123-456-789');
+
+    this.switchService = new Service.Switch(this.name);
+
+    this.switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.handleOnGet.bind(this));
+
+    this.pollForSpike();
+    setInterval(this.pollForSpike.bind(this), this.pollingInterval);
+  }
+
+  handleOnGet(callback) {
+    callback(null, this.spikeDetected);
+  }
+
+  async pollForSpike() {
+    try {
+      const response = await axios.get(this.apiUrl, {
+        headers: {
+          'accept': 'application/json',
+          'Authorization': `Bearer ${this.apiToken}`
+        }
+      });
+      const spikeStatus = response.data[0].spikeStatus; // Assuming the first item in the array
+      const spike = spikeStatus !== 'none';
+      if (spike && !this.spikeDetected) {
+        this.spikeDetected = true;
+        this.switchService.updateCharacteristic(Characteristic.On, true);
+        // Trigger HomeKit Automation
+      } else if (!spike && this.spikeDetected) {
+        this.spikeDetected = false;
+        this.switchService.updateCharacteristic(Characteristic.On, false);
+      }
+    } catch (error) {
+      this.log.error('Error fetching spike data:', error);
+    }
+  }
+
+  getServices() {
+    return [this.informationService, this.switchService];
+  }
+}
